c3371cbfd868fb409b6a8650a7e8e7e66a6ef641
Tuhin <towhid.tuhin@selise.ch>
2022-02-01 10:59:17 +0600

    Concurrency implementation

--- Program.cs
+++ Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Task.Delay(10000).Wait();
+//Console.WriteLine("Download File01!");
+
+//Task.Delay(10000).Wait();
+//Console.WriteLine("Download File02!");
+
+DownloadFile("File01");
+DownloadFile("File02");
+
+Console.WriteLine("Start data input, Enter your name");
+string str = Console.ReadLine();
+Console.WriteLine(str);
+
+static async void DownloadFile(string fileName)
+{
+    await Task.Delay(10000);
+    Console.WriteLine($"Download {fileName}!");
+}
